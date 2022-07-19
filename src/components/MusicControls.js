79c4921ea564771a115862fb397e73b690b8c164
nesatalha <83999950+nesatalha@users.noreply.github.com>
2022-07-20 01:18:40 +0300

    Update MusicControls.js

--- src/components/MusicControls.js
+++ src/components/MusicControls.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Slider from "@mui/material/Slider";
+import { ReactComponent as Line } from "../svgs/Dikdortgen.svg";
+import { ReactComponent as Play } from "../svgs/Oynat.svg";
+import { ReactComponent as Rewind } from "../svgs/Geri.svg";
+import { ReactComponent as Circle } from "../svgs/Halka.svg";
 
 const MusicControls = () => {
   return (
@@ -15,7 +19,29 @@ const MusicControls = () => {
         <div className="emptyDiv"></div>
 
         <div className="controls">
-          <div className="controlButtons">PLAY CONTROLS</div>
+          <div className="controlButtons">
+            <div className="rewindButton">
+              <span className="lineHelper">
+                <Line />
+              </span>
+              <Rewind />
+            </div>
+            <div className="playButton">
+              <div>
+                <Circle />
+              </div>
+              <div className="playButtonHelper">
+                <Play />
+              </div>
+            </div>
+            <div className="forwardButton">
+              <Play />
+              <span className="lineHelper">
+                <Line />
+              </span>
+
+            </div>
+          </div>
           <div className="controlBar">
             <Slider disabled small />
           </div>
